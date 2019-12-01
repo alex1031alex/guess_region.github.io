58b4d41e400378a13342cbbd4b0efb7e789b89ae
Alex <alex1031alex@yandex.ru>
2019-12-01 21:53:13 +0300

    Find tip template and clone

--- js/tip.js
+++ js/tip.js
@@ -1,1 +1,6 @@
-console.log('work');
+'use strict'
+
+const tipTemplate = document.querySelector('#tip').content.querySelector('.tip');
+const showTip = function () {
+	const tip = tipTemplate.cloneNode(true);
+}
