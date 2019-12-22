dd41349067cddb2dcd6f5b7b754fb0e1c8564ebf
Alex <alex1031alex@yandex.ru>
2019-12-22 23:04:00 +0300

    Add showTip function

--- js/tip.js
+++ js/tip.js
@@ -1,6 +1,28 @@
 'use strict'
 
+const SUCCES_MESSAGE = 'Вы угадали!';
+const MISTAKE_MESSAGE = 'Нет, это не он!';
+const TIP_SHOW_TIME = 500;
+const page = document.querySelector('body');
 const tipTemplate = document.querySelector('#tip').content.querySelector('.tip');
-const showTip = function () {
+
+const showTip = function (coordX, coordY, isRight) {
 	const tip = tipTemplate.cloneNode(true);
+	const tipText = tip.querySelector('.tip__text');
+
+	if (isRight) {
+		tipText.textContent = SUCCES_MESSAGE;
+	} else {
+		tipText.textContent = MISTAKE_MESSAGE;
+	}
+
+	tip.style.left = `${coordX}px`;
+	tip.style.top = `${coordY}px`;
+	page.appendChild(tip);
+
+	const tipRemove = function () {
+		tip.remove();
+	}
+
+	setTimeout(tipRemove, TIP_SHOW_TIME);
 }
