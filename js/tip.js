25b2c0302389614be9fa0fc597d511da9a9535e0
Alex <alex1031alex@yandex.ru>
2019-12-23 21:14:32 +0300

    Some comments in tip.js

--- js/tip.js
+++ js/tip.js
@@ -1,11 +1,15 @@
 'use strict'
-
+// Создадим всплывающие окна-подсказки
+// Сообщения в подсказках
 const SUCCES_MESSAGE = 'Вы угадали!';
 const MISTAKE_MESSAGE = 'Нет, это не он!';
+// Время показа подсказок
 const TIP_SHOW_TIME = 500;
+// Страница и шаблон с подсказками
 const page = document.querySelector('body');
 const tipTemplate = document.querySelector('#tip').content.querySelector('.tip');
 
+// Создадим функцию показывающую подсказки
 const showTip = function (coordX, coordY, isRight) {
 	const tip = tipTemplate.cloneNode(true);
 	const tipText = tip.querySelector('.tip__text');
