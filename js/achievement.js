5805aff58e0f124ddb7a1180ade3a2300536d5a8
Alex <alex1031alex@yandex.ru>
2020-02-16 19:57:16 +0300

    Change order of achivement addition

--- js/achievement.js
+++ js/achievement.js
@@ -3,7 +3,7 @@ const achivementTemplate = document.querySelector('#achievement')
 		.content.querySelector('.achievement-item');
 const containerLeft = document.querySelector('.achievement__left');
 const containerRight = document.querySelector('.achievement__right');
-let isOdd = false;
+let isOdd = true;
 
 const showAchievement = function (regionName) {
 	const block = achivementTemplate.cloneNode(true);
